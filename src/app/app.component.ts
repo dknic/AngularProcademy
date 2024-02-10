0aff7b7a16c7ffd42fbf1c7de2e6e489e7fa33d9
Deepak <dknic101993@gmail.com>
2024-02-10 14:30:37 +0530

    @outputDecorator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,11 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'This is a parant component';
-  // single value from parant to child
-  currentName = 'John Root';
-  obj = {
-    name: 'Kane Williamson',
-    age: 45,
-    country: 'New Zealand',
-  };
+  title = 'Parent Component';
+
+  users = ['Trent Boult', 'Devon Conway'];
+
+  addUser(user: string) {
+    this.users.push(user);
+  }
 }
