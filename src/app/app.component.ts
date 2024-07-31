ab2d2a9cec26cb4d7053787f2cd8de724dd6b914
Deepak <dknic101993@gmail.com>
2024-07-31 09:45:06 +0530

    reactive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'reactiveForm';
+  reactiveForm: FormGroup;
+  ngOnInit(): void {
+    this.reactiveForm = new FormGroup({
+      firstname: new FormControl(null),
+      lastname: new FormControl(null),
+      email: new FormControl(null),
+      username: new FormControl(null),
+      dob: new FormControl(null),
+      gender: new FormControl('female'),
+      street: new FormControl(null),
+      country: new FormControl('India'),
+      city: new FormControl(null),
+      region: new FormControl(null),
+      postal: new FormControl(null),
+    });
+  }
+
+  handleSubmit() {
+    console.log('button click');
+    console.log(this.reactiveForm);
+  }
 }
